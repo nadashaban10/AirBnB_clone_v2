dcf57857f68e7211dfadcf29cbabaca7cde022bf
nadashaban10 <nadashaban10@github.com>
2024-03-23 02:09:26 +0200

    task6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application:
+listening on 0.0.0.0, port 5000"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_bnb():
+    """Starts a Flask application"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Starts a Flask application"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def after_text(text):
+    """Starts a Flask application"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def after_python_text(text='is cool'):
+    """Starts a Flask application"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """Starts a Flask application"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def show_template(n):
+    """Starts a Flask application"""
+    return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def even_or_odd(n):
+    """Starts a Flask application"""
+    if n % 2 == 0:
+        return render_template('6-number_odd_or_even.html', num=n,
+                               even_odd='even')
+    else:
+        return render_template('6-number_odd_or_even.html', num=n,
+                               even_odd='odd')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
