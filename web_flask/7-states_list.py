bb160f26356acb0a068694bc308648152f82fc0b
nadashaban10 <nadashaban10@github.com>
2024-03-24 00:30:26 +0200

    task8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' update some part of our engine:'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state():
+    """Starts a Flask application"""
+    list_state = storage.all(State).values()
+    return render_template('7-states_list.html', list=list_state)
+
+
+@app.teardown_appcontext
+def teardown_db(execption):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
